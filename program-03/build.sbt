a8fb2b15a196cbd0ef2e8f8166b76bd44b76be12
James Cheuk <jccf091@gmail.com>
2016-07-24 13:30:15 +0800

    init commit

---
+++ program-03/build.sbt
@@ -0,0 +1,7 @@
+name := """program-03"""
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+fork in run := true
---
+++ program-03/src/main/scala/Program03.scala
@@ -0,0 +1,28 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Try, Success, Failure}
+
+object Program03 {
+  def main(args: Array[String]): Unit = {
+    println("run Program03!")
+
+    // I use Future becasue of it represents the result of an asynchronous computation.
+    val result: Future[Int] = Future {
+      1+1
+    }
+    // use onComplete to handle whether the Future returned
+    result.onComplete {
+        case Success(value) => println(value)
+        case Failure(e) => e.printStackTrace
+    }
+  }
+}
+
+
+/**
+  The java code use Thread from thread model,
+  and the synchronization is not implemented.
+  The variable result and hasResult was defined.
+  Inside the thread run method, the outter scope variable was mutated.
+  Setting hasResult as true to make program run is not a good practice.
+*/
